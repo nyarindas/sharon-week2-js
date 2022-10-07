712ebe08d45eec39fa18961dc9da0b29c13ad3fb
Sharon Nyarinda <nyarindas@gmail.com>
2022-10-07 17:36:43 +0300

    added first js lines

---
+++ JS/main.js
@@ -0,0 +1,3 @@
+const Days_of_the_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const Male_Names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const Female_Names = ["Äkosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
