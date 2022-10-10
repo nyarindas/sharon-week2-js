2ad06cd0716846aa1f13c85b23773d234dd32d29
Sharon Nyarinda <nyarindas@gmail.com>
2022-10-10 14:25:19 +0300

    added js lines of code,

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,45 @@
+// creating arrays for day & names
 const Days_of_the_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const Male_Names = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const Female_Names = ["Äkosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 
-var century = 19;
-var year = 1989;
-var month = 12;
-var day = 5;
-var gender = 'f'
+// var year = 1989;
+// var century = 19;
+// var month = 12;
+// var day = 5;
+// var gender = 'f'
 
 
 
-function calculateNameAndDay(century, year, month,day,gender){
-        var indexOfTheDay = ( ( (century/4) -2*century-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + day ) % 7;
+console.log(yearOfBirth);
 
-        var dayTheUserWasBorn = Days_of_the_week[indexOfTheDay];
-        var name = null;
+function calculateNameAndDay(century, year, month, day, gender){
+    var indexOfTheDay = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);       
+    var dayTheUserWasBorn =  Days_of_the_week[indexOfTheDay] 
+    var name = null;        
 
-        indexOfTheDay = Math.round(indexOfTheDay);
-
-        if (gender === 'm'){
+        if (gender === 'male'){
             name = Male_Names[indexOfTheDay];
-        } else if (gender === 'f'){
+        } else if (gender === 'female'){
             name = Female_Names[indexOfTheDay];
         } else{
             alert("Invalid gender, try again");
         }
         var list = [dayTheUserWasBorn, name];
+        return list;
+}
+
+ 
+function age (){
+    var userInputYear = parseInt(prompt("ConfirmEntertheYear:"));
+    var userInputMonth = parseInt(prompt("ConfirmEntertheMonth:"));
+    var userInputDay = parseInt(prompt("ConfirmEntertheDay:"));
+    var userInputGender = prompt("ConfirmEntertheGender:");
+    var userInputCentury = parseInt(userInputYear.toString().substring(0,2));
+
+    calculateNameAndDay(userInputCentury,userInputYear,userInputMonth,userInputDay, userInputGender)
 
 
-        console.log(dayTheUserWasBorn);
 }
 
